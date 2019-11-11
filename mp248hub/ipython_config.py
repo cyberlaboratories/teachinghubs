422d51af5c6f00882b370680d440435675ef7291
Falk Herwig <fherwig@uvic.ca>
2019-11-11 10:10:16 -0800

    updates for cyberhubs/mp248:1.0.0, add gtfs-realtime-bindings

--- mp248hub/ipython_config.py
+++ mp248hub/ipython_config.py
@@ -1,23 +1,20 @@
 c = get_config()
 
-#set matplotlib backend to nbagg
-c.InteractiveShellApp.matplotlib = "nbagg"
-
+#set matplotlib backend to inline
+#c.InteractiveShellApp.matplotlib = None
+#c.InteractiveShellApp.gui = None
+#c.InteractiveShellApp.pylab = "inline"
 c.InteractiveShellApp.exec_lines = [
-"%pylab inline\n",
-"%nbagg\n",
-"import sygma as s\n",
-"import omega as o\n",
-"from SYGMA import start_SYGMA\n",
-"from OMEGA import start_OMEGA\n",
-"from IPython.display import HTML\n",
-"display(HTML(\'<style>.container { width:80% !important; }</style>\'))\n"
+    "%pylab inline\n"
 ]
+# "display(HTML(\'<style>.container { width:80% !important; }</style>\'))
+#"%nbagg\n",
+#]
 
-c.NotebookNotary.data_dir="/home/user/.secret"
-c.NotebookNotary.db_file= "/home/user/.secret/nbsignatures.db"
-c.NotebookNotary.db_file= "/home/user/.secret/nbsignatures.db"
-c.NotebookApp.secret_file="/home/user/.secret/notebook_secret"
+#c.NotebookNotary.data_dir="/home/user/.secret"
+#c.NotebookNotary.db_file= "/home/user/.secret/nbsignatures.db"
+#c.NotebookNotary.db_file= "/home/user/.secret/nbsignatures.db"
+#c.NotebookApp.secret_file="/home/user/.secret/notebook_secret"
 
-c.NotebookApp.secret_file="/home/user/.local/share/jupyter/notebook_secret"
-c.update({"load_extensions":{"init_cell/main":True}})
+#c.NotebookApp.secret_file="/home/user/.local/share/jupyter/notebook_secret"
+#c.update({"load_extensions":{"init_cell/main":True}})
